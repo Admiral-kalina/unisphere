cacd027d7e1fc56a20936790d05b6485863b1c6c
Kaluna <kalinamono@gmail.com>
2022-07-31 10:50:10 +0300

    adapted 31.07

--- src/components/AboutTicker/AboutTicker.jsx
+++ src/components/AboutTicker/AboutTicker.jsx
@@ -9,6 +9,8 @@ const AboutTicker = () => {
             {() => (
                 <>
                     <h2 className={'ticker__element'}> ◌ Freedom Independence ◌ Intelligence ◌ Sociability ◌ Digitality Globalityindex  </h2>
+                    <h2  className={'ticker__element'} id={'ticker__element'}>  ◌ Intelligence ◌ Sociability ◌ Digitality Globalityindex ◌ Freedom Independence  </h2>
+                    <h2  className={'ticker__element'} id={'ticker__element'}>  ◌ Digitality Globalityindex ◌ Freedom Independence ◌ Intelligence ◌ Sociability  </h2>
                 </>
             )}
         </Ticker>
